f221dfc24e9553b0858813b9be77236b42312cd2
Dima <yudind@gmail.com>
2019-09-24 18:45:43 +0300

    disable per frame MOV printout

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
-set MCC=call mcc.bat
 mkdir build 2>nul
 :: Everything was fine with matlab 2015 and 2018, but now something goes wrong with mcc-2019a.
 :: > Undefined function or variable 'audioinfo' - ???
-%MCC% -m -a %~dsp0\PQevalAudio -I %~dsp0\PQevalAudio -d build PQevalAudio
+call mcc.bat -m -a %~dsp0\PQevalAudio -I %~dsp0\PQevalAudio -d build PQevalAudio
